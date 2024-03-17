8e3bddc61ef86165c12a3ba0901edfde69de3e57
phold <pholda@gmail.com>
2024-03-17 15:55:56 +0100

    Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './Header';
 import Login from './Login';
 import { Container } from 'react-bootstrap';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Profile from './Profile';
 
 function App() {
   return (
-    <Container className="container-lg">
-      <Header />
-      <Login />
-    </Container>
+    <Router>
+      <Container className="container-lg">
+        <Header />
+        <Routes>
+          <Route path="/login" Component={Login} />
+          <Route path="/profile" Component={Profile} />
+        </Routes>
+      </Container>
+    </Router>
   )
 };
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <NavbarBrand href="#">JWT UI</NavbarBrand>
+        <NavbarBrand as={Link} to="/">JWT UI</NavbarBrand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link href="#profile">Profile</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
